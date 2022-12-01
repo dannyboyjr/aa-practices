387b13c0e57f374657ccd361f2261ada2909d2fa
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2022-11-30 17:42:51 -0700

    react router navigation practice w27

---
+++ w27-wed-react-router-nav/src/App.js
@@ -0,0 +1,53 @@
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Stocks from './components/Stocks';
+import Movies from './components/Movies';
+import { Link, NavLink} from 'react-router-dom'
+import './index.css'
+
+function App() {
+  return (
+    <div className='main'>
+      <h1>App Component</h1>
+      <nav className="comp nav">
+        <ul>
+          <li>
+        <NavLink 
+        activeStyle={{ fontWeight: 'bold' }}
+        activeClassName='purple' 
+        exact to="/" >home</NavLink>
+          </li>
+          <li>
+        <NavLink 
+        activeStyle={{ fontWeight: 'bold' }}
+        activeClassName='purple' exact to="/stocks">stocks</NavLink>
+        </li>
+        <li>
+        <NavLink 
+        activeStyle={{ fontWeight: 'bold' }}
+        activeClassName='purple' exact to="/movies">movies</NavLink>
+        </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/stocks'>
+          <Stocks />
+        </Route>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route path='/not-logged-in'>
+          <h1> You Must Be Logged In To Enter.</h1>
+        </Route>
+        <Route>
+          <h1>Page Not Found</h1>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ w27-wed-react-router-nav/src/components/Stocks/index.js
@@ -0,0 +1,23 @@
+import { Redirect, useHistory } from 'react-router-dom'
+function Stocks() {
+  const history = useHistory()
+
+  const loggedIn = true
+  if(!loggedIn){
+    return <Redirect to='/not-logged-in' />
+  }
+
+  const handleClick = () =>{
+
+    window.alert('Sending info to the DB!');
+    return history.push('/')
+  }
+  return (
+    <div className='comp orange'>
+      <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
+    </div>
+  );
+}
+
+export default Stocks;
